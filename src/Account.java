86715cc986e8caced5ca6bb0008a06eec846cdeb
Scott <scottspeirsstirling@gmail.com>
2023-07-28 13:41:28 +0100

    Create account class.

---
+++ src/Account.java
@@ -0,0 +1,10 @@
+public class Account {
+    private int balance;
+    private String accountNumber;
+
+    public Account(String accountNumber, int balance){
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+
+    }
+}
