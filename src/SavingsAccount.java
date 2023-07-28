7873b90e529353ed9235695f2136dcb84b9f3135
Scott <scottspeirsstirling@gmail.com>
2023-07-28 17:12:39 +0100

    Create SavingsAccount.java

---
+++ src/SavingsAccount.java
@@ -0,0 +1,32 @@
+public class SavingsAccount implements AccountMethods {
+    private int balance;
+    private String accountNumber;
+
+    public SavingsAccount(String accountNumber, int balance){
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+
+    }
+
+    @Override
+    public void transfer(AccountMethods target, int money) {
+        //add code to allow transfer between accounts
+    }
+    public int checkBalance(){
+        return this.balance;
+    }
+    public void withdraw(int money){
+        if (this.getBalance() >= money){
+            balance -= money;
+        }
+    }
+    public void deposit(int money){
+        balance += money;
+    }
+    public String getAccountNumber(){
+        return accountNumber;
+    }
+    public int getBalance(){
+        return balance;
+    }
+}
