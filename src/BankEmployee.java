49ac81d42eb6deabe7b09463502569daf48665f2
Scott <scottspeirsstirling@gmail.com>
2023-07-28 23:44:48 +0100

    Add class BankEmployee with ability to create both types of accounts

---
+++ src/BankEmployee.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+public class BankEmployee {
+
+    public BankEmployee(){
+
+    }
+    public BasicAccount createBasicAccount(String accountNumber, Customer[] customers, int money){
+        return new BasicAccount(accountNumber, customers, money);
+
+    }
+    public SavingsAccount createSavingsAccount(String accountNumber, Customer[] customers, int money){
+        return new SavingsAccount(accountNumber, customers, money);
+
+    }
+
+}
