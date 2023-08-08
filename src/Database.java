7e6df1e2e78ede35e5555a0181ac7ad027c34d21
Scott <scottspeirsstirling@gmail.com>
2023-08-08 14:39:19 +0100

    change field accounts from non-static to static

--- src/Database.java
+++ src/Database.java
@@ -1,12 +1,15 @@
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask
 
 public class Database {
-    private ArrayList<AccountMethods> accounts = new ArrayList<AccountMethods>();
+    private static ArrayList<AccountMethods> accounts = new ArrayList<AccountMethods>();
 
     public static void addAccount(AccountMethods e){
         accounts.add(e);
     }
-    
+
+
 
 
 }
