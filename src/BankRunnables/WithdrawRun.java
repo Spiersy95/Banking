5a5913a6254058de5e8649a7cca9ff86e5ecaf2f
Scott <scottspeirsstirling@gmail.com>
2023-08-25 00:37:45 +0100

    make another class which implements runnable

---
+++ src/BankRunnables/WithdrawRun.java
@@ -0,0 +1,21 @@
+package BankRunnables;
+
+import Models.AccountMethods;
+import Models.Customer;
+
+public class WithdrawRun implements Runnable{
+
+    private Customer customer;
+    private AccountMethods account;
+    private int money;
+
+    public DepositRun(Customer customer, AccountMethods account, int money){
+        this.customer = customer;
+        this.account = account;
+        this.money = money;
+    }
+
+    public void run(){
+        customer.withdraw(account, money);
+    }
+}
