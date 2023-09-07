478957bff46ef002e009adaaa533a8521f272b64
Scott <scottspeirsstirling@gmail.com>
2023-09-07 15:08:45 +0100

    test which shows I do not lose track of the money while processing withdrawls, deposits and checking balances

--- src/Tests/GetBalanceTest.java
+++ src/Tests/GetBalanceTest.java
@@ -1,24 +1,33 @@
 package Tests;
 
+import BankRunnables.DepositRun;
 import BankRunnables.GetBalanceRun;
+import BankRunnables.WithdrawRun;
 import Models.BasicAccount;
 import Models.Customer;
 
 public class GetBalanceTest {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
 
         BasicAccount bs1 = new BasicAccount("001", 5000);
         Customer scott = new Customer("Scott");
         Customer elly = new Customer("elly");
+        scott.openAccount(bs1);
+        elly.openAccount(bs1);
         GetBalanceRun r1 = new GetBalanceRun(scott, bs1);
-        GetBalanceRun r2 = new GetBalanceRun(elly, bs1);
+        WithdrawRun r2 = new WithdrawRun(elly, bs1, 500);
+        DepositRun r3 = new DepositRun(scott, bs1 , 500);
 
-        Thread t1 = new Thread(r1);
-        Thread t2 = new Thread(r2);
+        Thread t1 = new Thread(r3);
+        Thread t2 = new Thread(r1);
+        Thread t3 = new Thread(r2);
+
+
 
         t1.start();
         t2.start();
+        t3.start();
 
 
     }
