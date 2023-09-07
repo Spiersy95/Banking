eb8d10f32cd7f7c13a934eaf66ea8be25be6f8ab
Scott <scottspeirsstirling@gmail.com>
2023-09-07 15:09:44 +0100

    Add a sleep to help with testing

--- src/BankRunnables/GetBalanceRun.java
+++ src/BankRunnables/GetBalanceRun.java
@@ -2,18 +2,26 @@ package BankRunnables;
 
 import Models.Customer;
 import Models.AccountMethods;
-public class GetBalanceRun implements Runnable{
+public class GetBalanceRun implements Runnable {
 
     private Customer customer;
     private AccountMethods account;
-    public GetBalanceRun(Customer customer, AccountMethods account){
+
+    public GetBalanceRun(Customer customer, AccountMethods account) {
         this.customer = customer;
         this.account = account;
     }
 
-    public void run(){
-        customer.getBalance(account);
-        Thread.sleep(1);
+    public void run() {
+        try{
+            System.out.println(customer.getBalance(account));
+            Thread.sleep(1);
+        } catch (InterruptedException e){
+            System.out.println("Sorry this thread was interrupted");
+        }
+
+
     }
+
 
 }
