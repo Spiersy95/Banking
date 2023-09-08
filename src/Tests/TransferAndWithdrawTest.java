266d09d673d34108ab11c5f59206d8e65d3d8b1f
Scott <scottspeirsstirling@gmail.com>
2023-09-08 12:37:02 +0100

    create Class to test the transfer and withdraw class together

--- src/Tests/TransferAndWithdrawTest.java
+++ src/Tests/TransferAndWithdrawTest.java
@@ -1,5 +1,28 @@
 package Tests;
 
+import BankRunnables.TransferRun;
+import BankRunnables.WithdrawRun;
+import Models.BasicAccount;
+import Models.Customer;
+
 public class TransferAndWithdrawTest {
 
+    public static void main(String[] args){
+
+        BasicAccount bs1 = new BasicAccount("001", 5000);
+        BasicAccount bs2 = new BasicAccount("002", 5000);
+        Customer scott = new Customer("Scott");
+        Customer elly = new Customer("elly");
+        scott.openAccount(bs1);
+        elly.openAccount(bs2);
+
+        TransferRun r1 = new TransferRun(scott, bs1, bs2, 5000);
+        WithdrawRun r2 = new WithdrawRun(scott, bs1, 1);
+
+        Thread t1 = new Thread(r1);
+        Thread t2 = new Thread(r2);
+
+        t1.start();
+        t2.start();
+    }
 }
